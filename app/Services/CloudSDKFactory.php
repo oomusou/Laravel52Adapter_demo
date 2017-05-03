44eae61307c82b4f9628e4b4f8cf17ed0e56b43a
SamXiaoSenao <sam.xiao@senao.com.tw>
2017-05-03 16:18:43 +0800

    CloudSDKFactory

--- app/Services/CloudSDKFactory.php
+++ app/Services/CloudSDKFactory.php
@@ -4,8 +4,18 @@ namespace App\Services;
 
 class CloudSDKFactory
 {
+    /**
+     * 建立 adapter
+     * @return CloudSDK
+     */
     public static function create(): CloudSDK
     {
+        $lut = config('app.CloudLUT');
+        $cloudStorage = config('app.CloudStorage');
 
+        $className = collect($lut)
+            ->get($cloudStorage, AWSAdapter::class);
+
+        return new $className;
     }
 }
